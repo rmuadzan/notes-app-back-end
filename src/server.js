71959504b99f369db4ec85afdcec0600ffaf4691
Rafli Muadzan <raflimuadzan2@gmail.com>
2023-04-15 11:38:46 +0800

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesServices');
 const NotesValidator = require('./validator/notes');
 const users = require('./api/users');
 const UsersValidator = require('./validator/users');
 const UsersServices = require('./services/postgres/UsersServices');
+const authentications = require('./api/authentications');
+const AuthenticationsValidator = require('./validator/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsServices');
+const TokenManager = require('./tokenize/tokenManager');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersServices();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,6 +26,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -35,6 +63,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
